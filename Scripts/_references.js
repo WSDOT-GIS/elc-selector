6128c07d7877ec546341deb6577c4a3e78f68095
JeffJacobson <JeffJacobson@users.noreply.github.com>
2014-04-04 13:00:50 -0700

    Removed elc-js submodule. Code is now directly in repository for gh-pages version.

--- Scripts/_references.js
+++ Scripts/_references.js
@@ -1,5 +1,4 @@
-﻿/// <autosync enabled="true" />
+﻿// <autosync enabled="true" />
 /// <reference path="routelocator.js" />
 /// <reference path="../demos/scripts/demo.js" />
-/// <reference path="elc-js/elc.js" />
 /// <reference path="elc-arcgis-utils.js" />
